6c385f67ec563bccca456f5e8de3827a3cf91d92
mambzero <mambzero@abv.bg>
2019-03-31 18:06:01 +0300

    first commit - basic view

--- index.php
+++ index.php
@@ -4,10 +4,26 @@
         <title>Simple Regex Matcher</title>
         <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" />
         <link href="css/style.css" rel="stylesheet" />
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/d3js/5.7.0/d3.min.js"></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     </head>
     <body>
-        
+        <div class="container" id="page">
+            <div class="row">
+                <h1>Simple Regex Matcher</h1>
+            </div>
+            <div class="row input-field">
+                <div class="col-md-8">
+                    <input name="regex" class="form-control" />
+                </div>
+            </div>
+            <div class="row textarea-field">
+                <div class="col-md-8">
+                    <textarea name="string" class="form-control"></textarea>
+                </div>
+            </div>
+        </div>
+        <script>
+            $('body').css({ height: $(document).height()});
+        </script>
     </body>
 </html>
