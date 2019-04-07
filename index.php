82bee94fb0380d4eb9348c97d12edac26df0577f
mambzero <mambzero@abv.bg>
2019-04-07 18:46:09 +0300

    fixed html structure

--- index.php
+++ index.php
@@ -11,19 +11,19 @@
     <body>
         <div class="main-wrapper">
             <div class="section left col-md-9">
-			<div class="header">
-                <h1>Simple Regex Matcher</h1>
-				<div class="dropdown">
-					<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
-						<span class="caret"></span>
-					</button>
-					<ul class="dropdown-menu dropdown-menu-right" id="menu">
-					  <li><a href="#" class="options selected" id="opt1">preg_match_all</a></li>
-					  <li><a href="#" class="options" id="opt2">preg_match</a></li>
-					  <li><a href="#" class="options" id="opt3">preg_split</a></li>
-					</ul>
+				<div class="header">
+					<h1>Simple Regex Matcher</h1>
+					<div class="dropdown">
+						<button class="btn btn-primary dropdown-toggle" type="button" data-toggle="dropdown">
+							<span class="caret"></span>
+						</button>
+						<ul class="dropdown-menu dropdown-menu-right" id="menu">
+						  <li><a href="#" class="options selected" id="opt1">preg_match_all</a></li>
+						  <li><a href="#" class="options" id="opt2">preg_match</a></li>
+						  <li><a href="#" class="options" id="opt3">preg_split</a></li>
+						</ul>
+					</div>
 				</div>
-			</div>
                 <input type="text" name="regex" class="form-control input-field" id="regex" />
                 <textarea type="text" name="string" class="form-control textarea-field" id="string"></textarea>
 				<input type="hidden" id="preg_func" value="preg_match_all" />
