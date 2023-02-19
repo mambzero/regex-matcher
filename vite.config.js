b3d7d1ae85e18887e98aac651562a54f41fd5358
mambzero <mambzero@abv.bg>
2023-02-19 03:33:01 +0200

    Setup deploy to gh-pages

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,23 @@
-const path = require('path');
+import { defineConfig, loadEnv } from 'vite';
+import path from 'path';
 
-export default {
-  root: path.resolve(__dirname, 'src'),
-  publicDir: '../public',
-  build: {
-    outDir: '../dist'
-  },
-  resolve: {
-    alias: {
-      '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    base: env.BASE_URL,
+    root: path.resolve(__dirname, 'src'),
+    publicDir: '../public',
+    build: {
+      outDir: '../dist'
+    },
+    resolve: {
+      alias: {
+        '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
+      }
+    },
+    server: {
+      port: 3000,
+      hot: true
     }
-  },
-  server: {
-    port: 3000,
-    hot: true
   }
-}
+});
