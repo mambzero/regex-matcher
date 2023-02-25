73198c8f535ed8e785506cb4bb4da72d3f001ef7
mambzero <mambzero@abv.bg>
2023-02-25 08:37:23 +0200

    Deploy

--- assets/index-30ba913a.js
+++ assets/index-30ba913a.js
@@ -2,11 +2,11 @@
   <div class="wrapper container-fluid">
     <div class="header row bg-primary">
       <div class="col-12">
-        <div class="d-flex flex-nowrap align-items-center">
+        <div class="d-flex flex-wrap flex-sm-nowrap align-items-center">
           <img class="logo vanila" src="images/javascript.svg" alt="Regex 99 logo">
           <h1>Regex 99</h1>
           <div class="input-group">
-            <input type="text" name="pattern" class="form-control ms-3" placeholder="Pattern">
+            <input type="text" name="pattern" class="form-control ms-sm-3" placeholder="Pattern">
             <div class="input-group-text">
               <div class="dropdown" id="flags">
                 <a class="dropdown-toggle" type="button" id="flags-dropdown-btn" data-bs-toggle="dropdown" aria-expanded="false">
@@ -39,7 +39,7 @@
             <a href="https://vitejs.dev" target="_blank">
               <img src="images/vite.svg" alt="Vite logo" class="mb-1" height="20px" width="auto" />
             </a>
-          </a>
+          </p>
         </div>
       </div>
     </div>
